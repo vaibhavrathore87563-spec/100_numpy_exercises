9e6bc63b890bc391dab469e8f393d9a6ce7cf700
vaibhavrathore87563-spec <tumhara_github_email>
2026-06-24 23:56:33 +0530

    code9th

---
+++ 9_problem.py
@@ -0,0 +1,13 @@
+# create a vector with values ranging from 10 to 49
+import numpy as np
+
+ranging = np.arange(10,50)
+print(ranging)
+
+
+# create a vector with values ranging from 10 to 999
+
+import numpy as np
+
+ranging = np.arange(10,1000)
+print(ranging)
