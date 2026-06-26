18b8734822f0dc90aa3da66e6b10137be3ba771b
vaibhavrathore87563-spec <tumhara_github_email>
2026-06-26 17:18:41 +0530

    10 x10 chessboard using tile

---
+++ 35_problem.py
@@ -0,0 +1,6 @@
+#Using only np.tile(), create a 10 × 10 chessboard pattern starting with 0 in the top-left corner.
+
+import numpy as np
+arr = np.tile([[0,1],
+               [1,0]],(5,5))
+print(arr)
