14f3c4ba9f9b4be2357bf5e865c030ec7183715b
vaibhavrathore87563-spec <vaibhavrathore87563@gmail.com>
2026-06-27 00:10:41 +0530

    Find common value between two arrays

--- 44_problem.py
+++ 44_problem.py
@@ -1,5 +1,10 @@
+
+#How to find common values between two arrays?
+
 import numpy as np
 a= np.array([1,2,2,3,4])
 b = np.array([2,2,4,5])
 
 print(np.intersect1d(a,b))
+
+ #gitintersect1d() --> Returns unique and common value
