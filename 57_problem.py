44db09125b97dbbf6c6add8bd003a08f991dc53c
vaibhavrathore87563-spec <vaibhavrathore87563@gmail.com>
2026-06-27 23:39:13 +0530

    Equally Spaced Value Linspaced()

--- 57_problem.py
+++ 57_problem.py
@@ -3,3 +3,11 @@
 import numpy as np
 arr = np.linspace(1,1000,12)
 print("value between 1 to 1000",arr)
+
+
+
+#Create a vector of size 18 with values ranging from 25 to 250, both excluded.
+
+import numpy as np
+arr = np.linspace(25,250,20)[1:-1]
+print("value between 25 to 250",arr)
