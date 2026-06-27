647ba758d83252fbab5b04f833baf1d5c3890f24
vaibhavrathore87563-spec <vaibhavrathore87563@gmail.com>
2026-06-27 23:16:14 +0530

    Generate equally spaced value using linspace()

---
+++ 56_problem.py
@@ -0,0 +1,23 @@
+#Create a vector of size 8 with values ranging from 5 to 50, both excluded.
+
+import numpy as np
+arr = np.linspace(5,50,10)[1:-1]
+
+print(arr)
+
+#Create a vector of size 6 with values ranging from 20 to 80, both excluded.
+
+import numpy as np
+
+arr = np.linspace(20,80,8)[1:-1]
+
+print(arr)
+
+
+#Create a vector of size 20 with values ranging from -50 to 50, both excluded.
+
+import numpy as np
+
+arr = np.linspace(-50,50,22) [1:-1]
+
+print(arr)
