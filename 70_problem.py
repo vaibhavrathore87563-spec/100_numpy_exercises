c0cee64487b11330b9fb48e4758a41452d476f31
vaibhavrathore87563-spec <vaibhavrathore87563@gmail.com>
2026-06-29 23:34:41 +0530

    Data type Information

---
+++ 70_problem.py
@@ -0,0 +1,11 @@
+# Print the minimum and maximum representable values for each numpy scalar type
+
+import numpy as np
+for dtype in [np.int8, np.int16, np.int32 ,np.int64]:
+    print(dtype)
+print("Min:", np.iinfo(dtype).min)
+print("Max:", np.iinfo(dtype).max)
+print()
+
+
+#iinfo()-->means integer information.It returns details about the integer type.
