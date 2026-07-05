af4b1efb582c3bf06cd94c1b02eeeca738335b85
vaibhavrathore87563-spec <vaibhavrathore87563@gmail.com>
2026-07-05 16:33:43 +0530

    Extract Contiguous 3X3 blocks from a random array

--- 105_problem.py
+++ 105_problem.py
@@ -1,11 +1,16 @@
 # Extract all the contiguous 3x3 blocks from a random 10x10 matrix
 
 
-import numpy as np
+import numpy as np 
 
-from numpy.lib.stride_tricks import sliding_window_view
-
-arr = np.random.randint(0,10, (10,10))
+from numpy .lib.stride_tricks import sliding_window_view
+arr = np.random.randint(0, 10, (10,10))
 print(arr)
-blocks = sliding_window_view (arr, (3,3))
+
+blocks = sliding_window_view(arr, (3,3))
 print(blocks.shape)
+#           O/P
+# 8 possible row positions
+# 8 possible column positions
+# 3 rows inside each block
+# 3 columns inside each block
