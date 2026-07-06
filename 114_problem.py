4c35b4e757157ffd7a620638e3856b27da9493bf
vaibhavrathore87563-spec <vaibhavrathore87563@gmail.com>
2026-07-06 23:37:14 +0530

    Element-wise Exponentiation

---
+++ 114_problem.py
@@ -0,0 +1,6 @@
+# Element-wise Exponentiation
+import numpy as np
+arr = np.array([1,2,3,4,5])
+print(np.power(arr,2))
+
+#np.power(arr, 2) raises each element of the array to the power of 2 (square).
